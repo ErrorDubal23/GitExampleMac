6b8504f7ce93371e12e3a96b2f0742642f900836
Dubal <dubala@uninorte.edu.co>
2025-04-02 00:54:19 -0500

    Implemetacion feature 2

---
+++ src/core/Dueño.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author dubalaguilar
+ */
+public class Dueño {
+    private String nombre;
+    private String apellido;
+    private String direccion;
+
+    public Dueño(String nombre, String apellido, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+    }
+    
+    
+}
